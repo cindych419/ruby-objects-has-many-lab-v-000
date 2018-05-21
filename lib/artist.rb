820d3195669c2f8ebe56074d99596006d49054ec
cindych419@gmail.com <cindych419@gmail.com>
2018-05-21 23:25:06 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,11 @@
 class Artist
+
   attr_accessor :name
-  def initialize(name)
-    @name = name
-    @songs = []
-  end
+
+def initialize(name)
+  @name = name
+  @songs = []
+end
 
 def add_song(song)
   @songs << song
@@ -13,4 +15,10 @@ end
 def songs
   @songs
 end
+
+def add_song_by_name(name, genre)
+  song = Song.new(name, genre)
+  @songs<<song
+  song.artist = self
 end
+end 
